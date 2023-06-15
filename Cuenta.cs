56e9f65a4cfcde930e906832f583bfd85aa3867b
alexx73 <tristanluna@yahoo.com.ar>
2023-06-15 15:31:35 -0300

    Agregar archivos de proyecto.

---
+++ Cuenta.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ejemploCuenta
+{
+    class Cuenta
+    {
+       private double saldo = 0.0;
+        private int cOperaciones = 0;
+        private int nroCuenta;
+       
+        public Cuenta(int cuenta,double monto)
+        {
+            saldo = monto;
+            nroCuenta = cuenta;
+        }
+         
+        public double Saldo
+        {
+            get
+            {
+                //coperaciones = 0; 
+                return saldo;
+            }
+            set
+            {
+                saldo = value;
+            }
+        }
+        public bool Extraccion(double monto)
+        {
+         bool exito= false;
+        
+         if (monto<=saldo-1.5)
+            {
+                cOperaciones++;
+                if (cOperaciones >5)
+                  {
+                   saldo -= 1.5;
+                  }
+                saldo = saldo - monto;
+                exito = true;
+            }
+         return exito; 
+        }
+        public void Deposito(double monto)
+        {
+           cOperaciones++;
+           if (cOperaciones > 5)
+            {
+                saldo -= 1.5;
+            }
+            saldo = saldo + monto;
+        }
+        public int Operaciones()
+        {
+            return cOperaciones;
+        } 
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemploCuenta
+{
+    public partial class F_principal : Form
+    {
+        Cuenta miCuenta;
+        public F_principal()
+        {
+            InitializeComponent();
+             miCuenta = new Cuenta(0010, 5000);
+            double montoExtraer;
+        }
+
+        private void F_principal_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double montoExtraccion;
+            F_secundaria vExtraer = new F_secundaria();
+
+            // abre ventana secundaria
+
+            if (vExtraer.ShowDialog() == DialogResult.OK )
+            {
+                montoExtraccion = Convert.ToDouble(vExtraer.textBox2.Text) ;
+                if (miCuenta.Extraccion(montoExtraccion))
+                {
+
+                    //label1_Click.Text = "tt";
+                    label1.Text = "exito";
+                }
+                else
+                {
+                    label1.Text = "error en la operacion";
+                }
+
+            } else
+            {
+                MessageBox.Show("cancelado por el usuario");
+            }
+
+            vExtraer.Dispose();
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            miCuenta.Deposito(3000);
+        }
+
+       
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text =  miCuenta.Saldo.ToString("$ 0.00");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
